3422bdd5feb33baacf4d496ac8cda8bbac6d44de
christianleng <christian.leng@epitech.eu>
2025-01-31 14:49:24 +0100

    feat(): deploy github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,18 @@
 import withMDX from '@next/mdx';
 
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig = withMDX({
   extension: /\.mdx$/,
-  options: {
-  },
+  options: {},
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
 });
 
 export default {
   ...nextConfig,
   reactStrictMode: true,
+  assetPrefix: isProd ? "/portfolio/" : "", 
+  images: {
+    unoptimized: true,
+  },
+  output: "export", 
 };
