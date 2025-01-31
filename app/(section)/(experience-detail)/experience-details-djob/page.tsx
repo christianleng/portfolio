6f226e4dd702f1963bdfcaab8f99b6a5cc64d0af
christianleng <christian.leng@epitech.eu>
2025-01-31 15:55:36 +0100

    feat(): create page detail

--- app/(section)/(experience-detail)/experience-details-djob/page.tsx
+++ app/(section)/(experience-detail)/experience-details-djob/page.tsx
@@ -4,18 +4,18 @@ import Link from "next/link";
 import { notFound } from "next/navigation";
 import React from "react";
 
-export default async function ExperienceEatsEazy() {
-  const post = await getPostBySlug("eatseazy");
+export default async function ExperienceDjob() {
+  const post = await getPostBySlug("experience-details-djob");
   if (!post) notFound();
 
   return (
-    <div className="pe-8 ps-8 prose mx-auto">
+    <div className="pt-9 pe-8 ps-8 prose mx-auto" id="experience">
       <MDXRemote source={post.content} />
       <Link
-        href={`/experiences/${post.url}`}
+        href={"/#experience"}
         className="text-greenShadow4 hover:text-greenShadow3"
       >
-        Voir plus sur les missions réalisées
+        Retour
       </Link>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
-import { getPosts } from "@/lib/loadSections";
 import About from "./(section)/about/page";
 import Homepage from "./(section)/home/page";
-import ExperienceDjob from "./(section)/experience/(experience-djob)/page";
-import ExperienceEatsEazy from "./(section)/experience/(experience-eatseazy)/page";
+import ExperienceDjob from "./(section)/(experience)/experience-djob/page";
+import ExperienceEatsEazy from "./(section)/(experience)/experience-eatseazy/page";
 import SkillsPage from "./(section)/skills/page";
 import Contact from "./(section)/contact/page";
 
 export default async function Home() {
-  const posts = await getPosts();
-
   return (
     <div className="flex flex-col gap-36">
       <Homepage />
